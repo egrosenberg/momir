f873852fd78911592cc1d82521c6ddd269919656
Ethan Rosenberg <erosenberg2019@hotmail.com>
2025-06-23 18:57:01 -0700

    removed sliceImage (obsolete)

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from .fetch_json import fetchJson
 from .random_by_mv import filterByMv, randomByMv
 from .get_image import getArt
-from .slice_image import sliceImage
 
-__all__ = ['fetchJson', 'filterByMv', 'randomByMv', 'getArt', 'sliceImage']
+__all__ = ['fetchJson', 'filterByMv', 'randomByMv', 'getArt',]
