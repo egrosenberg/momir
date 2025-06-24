862414f3dc8c928ed834f693dda6beb5f36a5af0
Ethan Rosenberg <erosenberg2019@hotmail.com>
2025-06-23 19:51:29 -0700

    added printCard to init

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .fetch_json import fetchJson
 from .random_by_mv import filterByMv, randomByMv
 from .get_image import getArt
+from .print_card import printCard
 
-__all__ = ['fetchJson', 'filterByMv', 'randomByMv', 'getArt',]
+__all__ = ['fetchJson', 'filterByMv', 'randomByMv', 'getArt', 'printCard']
