b212ed8a09a8b4f7eb9b225cce269984b4d7e9e3
Ethan Rosenberg <erosenberg2019@hotmail.com>
2025-06-24 15:56:54 -0700

    moved momir except keyboardinterrupt

--- src/momir.py
+++ src/momir.py
@@ -5,16 +5,16 @@ def momir():
     printer = mprint.getPrinter()
     cards = mprint.fetchJson(creaturesOnly=True)
     print("Entering Momir Mode - Enter 'e' or 'escape' at any time to stop")
-    while True:
-        n = input("Enter a number between 0 and 16: ")
-        if n == "e" or n == "exit":
-            break
-        try:
-            card = mprint.randomByMv(cards, int(n))
-            mprint.printCard(printer, card)
-        except ValueError:
-            print("Invalid input. If you would like to exit, enter 'e'.")
-        except KeyboardInterrupt:
-            return mprint.closePrinter()
-
+    try:
+        while True:
+            n = input("Enter a number between 0 and 16: ")
+            if n == "e" or n == "exit":
+                break
+            try:
+                card = mprint.randomByMv(cards, int(n))
+                mprint.printCard(printer, card)
+            except ValueError:
+                print("Invalid input. If you would like to exit, enter 'e'.")
+    except KeyboardInterrupt:
+        return mprint.closePrinter()
     mprint.closePrinter()
